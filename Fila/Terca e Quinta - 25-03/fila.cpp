6a5dbe8a44c0e45afce7f047e25a75569307c7d6
Mário Sérgio Cavalcante <55106614+MarioSergioCavalcante@users.noreply.github.com>
2025-03-27 23:32:05 -0400

    Adicionando o final da implementação da fila

--- Fila/Terca e Quinta - 25-03/fila.cpp
+++ Fila/Terca e Quinta - 25-03/fila.cpp
@@ -11,7 +11,10 @@ bool estaVazia(int qtd){
 }
 
 void imprimeFila(int *fila, int frente, int quantidade){
-
+    if(quantidade == 0){
+        cout << "Fila vazia\n"; 
+        return;
+    }
     int indice = frente;
     for(int i = 0; i < quantidade; i++){
         cout << fila[indice] << " -> "; 
@@ -20,17 +23,66 @@ void imprimeFila(int *fila, int frente, int quantidade){
     cout << endl;
 }
 
+void enfileira(int *fila, int *qtd, int *tras, int valor){
+    if(estaCheia(*qtd)){
+        cout << "Não é possivel inserir nenhum elemento, porque a fila esta cheia" << endl;
+        return; // return em função funciona como um break, para a execução da função
+    }
+    fila[(*tras)] = valor;
+    (*tras) = ((*tras) + 1)  % TAM;
+    (*qtd)++;
+}
+
+void desinfileira(int *fila, int *qtd, int *frente){
+    if(estaVazia(*qtd)){
+        cout << "Não possivel retirar nenhum elemento" << endl;
+        return;
+    }
+    int auxiliar = fila[*frente];
+    cout << "O valor retirado foi: " << auxiliar << endl;
+    (*frente) = ((*frente) + 1) % TAM;
+    (*qtd)--;
+}
+
+
 int main(){
     system("cls");
-    int fila[TAM] = {1,2,3,4,5};
+    int fila[TAM];
     int frente,  tras;
-    int quantidade = 5;
+    int quantidade = 0;
     
-    frente = tras = 0; //C++ 
-    frente = 2;
-    tras = 1;
+    frente = 0;
+    tras = 0;
     cout << "Esta vazia? " << estaVazia(quantidade) << endl;
     cout << "Esta cheia? " << estaCheia(quantidade) << endl;
     imprimeFila(fila, frente,quantidade);
+    cout << "Acrescentando o elemento 10: "<< endl; 
+    enfileira(fila, &quantidade, &tras, 10);
+    imprimeFila(fila, frente, quantidade);
+    enfileira(fila, &quantidade, &tras, 22);
+    imprimeFila(fila, frente, quantidade);
+    enfileira(fila, &quantidade, &tras, -10);
+    imprimeFila(fila, frente, quantidade);
+    enfileira(fila, &quantidade, &tras, -31);
+    imprimeFila(fila, frente, quantidade);
+
+    enfileira(fila, &quantidade, &tras, 90);
+    imprimeFila(fila, frente, quantidade);
+
+    desinfileira(fila, &quantidade, &frente);
+    imprimeFila(fila, frente, quantidade);
+
+    enfileira(fila, &quantidade, &tras, 0);
+    imprimeFila(fila, frente, quantidade);
+
+    desinfileira(fila, &quantidade, &frente);
+    imprimeFila(fila, frente, quantidade);
+
+
+    desinfileira(fila, &quantidade, &frente);
+    imprimeFila(fila, frente, quantidade);
+
+
+
     return 0;
 }
